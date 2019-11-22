026080a214ac57844a599ef893e89f91b9f93c89
LeuvaApurv <57341831+LeuvaApurv@users.noreply.github.com>
2019-11-22 14:57:17 -0500

    Add files via upload

---
+++ speed_up_kali_update.sh
@@ -0,0 +1,20 @@
+echo "---------Speed up Kali Linux Update----------"
+echo
+echo "------------------------------------------------------------------------------"
+echo "#########          speed_up_kali_update.sh - By Leuva Apurv          #########"
+echo "###         https://github.com/LeuvaApurv/speed_up_kali_update.sh          ###"
+echo "------------------------------------------------------------------------------"
+echo
+echo "[+] Backup orignal sources.list to sources.list.bak and create new sources.list" 
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo touch /etc/apt/sources.list
+echo
+echo "[+] Add best kali repo in sources.list"
+echo "# speed_up_kali_update.sh - By Leuva Apurv" | sudo tee -a /etc/apt/sources.list
+echo "deb http://kali.download/kali kali-rolling main contrib non-free" | sudo tee -a /etc/apt/sources.list
+echo
+echo "[+] Update or Upgrade kali"
+sudo apt clean
+sudo apt update && apt upgrade -y && apt dist-upgrade -y
+echo
+echo "---------------Suceessfullly Update your Kali Linux----------------"
